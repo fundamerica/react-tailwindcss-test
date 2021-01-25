1e9fb7d2a0487783dc58e0718714298891d45d47
Teddy Stanowski <tstanowski31@gmail.com>
2021-01-25 13:32:02 -0800

    fix color styling for submit button to match colors from config file

--- src/utilities/buttons/styles/Buttons.js
+++ src/utilities/buttons/styles/Buttons.js
@@ -1,19 +1,19 @@
 import tw, { styled } from 'twin.macro';
 
 export const Submit = styled.button`
-	${tw`bg-blue rounded-lg active:bg-green`}
+	${tw`rounded-lg bg-primeGreen`};
 	padding: 1rem 2rem;
 	margin: 1rem auto;
 `;
 
 export const Login = styled.button`
-	${tw`bg-green rounded-lg active:bg-blue`}
+	${tw`bg-blue rounded-lg`};
 	padding: 1rem 2rem;
 	margin: 1rem auto;
 `;
 
 export const Reset = styled.button`
-	${tw`bg-stone rounded-lg`}
+	${tw`bg-stone rounded-lg`};
 	color: white;
 	padding: 1rem 2rem;
 	margin: 1rem auto;
