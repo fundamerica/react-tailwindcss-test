55cc918b73188f29002ba7672d8fa82c4625f9a0
Teddy Stanowski <tstanowski31@gmail.com>
2021-01-26 14:24:29 -0800

    add tailwindui components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Login, Submit, Reset } from './utilities/buttons/styles/Buttons';
 import { Theme } from './utilities/themes/PrimeTheme';
 import styled from 'styled-components';
+import Tables from './components/tables/Tables';
+import MultiLayout from './components/layouts/MultiLayout';
 
 function App() {
 	return (
-		<div className='App'>
+		<>
 			<Theme>
 				<Container>
 					<Heading isHeading={true}>Hello World</Heading>
@@ -18,25 +20,36 @@ function App() {
 					</Paragraph>
 				</Container>
 			</Theme>
-			<div className='App-header'>
-				<div className='border-div'>
-					<h3>Submit Button from buttons folder</h3>
-					<Submit type='submit'>Submit</Submit>
-				</div>
-				<div className='border-div'>
-					<h3>Login Button from buttons folder</h3>
-					<Login type='button'>Login Here</Login>
-				</div>
-				<div className='border-div'>
-					<h3>Reset Button</h3>
-					<Reset>Reset Button</Reset>
+			<h1 style={{ textAlign: 'center', fontSize: '42pt' }}>
+				Tailwind UI Data Table example
+			</h1>
+			<Tables />
+			<h1 style={{ textAlign: 'center', fontSize: '42pt' }}>
+				Tailwind UI Application Shell Layout
+			</h1>
+			<MultiLayout />
+			<div className='App'>
+				<div className='App-header'>
+					<div className='border-div'>
+						<h3>Submit Button from buttons folder</h3>
+						<Submit type='submit'>Submit</Submit>
+					</div>
+					<div className='border-div'>
+						<h3>Login Button from buttons folder</h3>
+						<Login type='button'>Login Here</Login>
+					</div>
+					<div className='border-div'>
+						<h3>Reset Button</h3>
+						<Reset>Reset Button</Reset>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
 const Container = styled.div`
+	text-align: center;
 	max-width: 100%;
 	border: ${(props) => `15px solid ${props.theme.colors.stone}`};
 	background-color: ${(props) => props.theme.colors.primeBlue};
